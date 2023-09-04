406dc9af7330b96467a5a7d525d1c86781006a89
Jakub Jančík <jjancik@digitalsystems.eu>
2023-09-04 15:03:18 +0200

    icon table fix

--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -1,13 +1,15 @@
 <template>
-    <v-data-table mobile-breakpoint="0" hide-default-footer :headers="headers" :items="teams" :items-per-page="24" class="elevation-0">
+    <v-data-table mobile-breakpoint="0" hide-default-footer :headers="headers" :items="teams" :items-per-page="24"
+        class="elevation-0">
         <template #[`item.score`]="{ item }">
             {{ item.score[0] + ':' + item.score[1] }}
         </template>
-        <template #[`item.logo`]="{ item }">
+        <!-- <template #[`item.logo`]="{ item }">
             <v-img max-height="20" max-width="20" :src="item.logo" />
-        </template>
+        </template> -->
         <template #[`item.club_name`]="{ item }">
-            <div :class="favTeams.includes(item.club_name) ? 'font-weight-bold' : ''">
+            <div :class="favTeams.includes(item.club_name) ? 'font-weight-bold d-inline-flex' : 'd-inline-flex align-center'">
+            <v-img class="mr-2" max-height="20" max-width="20" :src="item.logo" />
                 {{ item.club_name }}
             </div>
         </template>
@@ -22,7 +24,7 @@
 <script>
 
 function compareFn(a, b) {
-    return b.points - a.points || new Date(b.score[0]-b.score[1]) - new Date(a.score[0]-a.score[1])
+    return b.points - a.points || new Date(b.score[0] - b.score[1]) - new Date(a.score[0] - a.score[1])
 }
 
 export default {
@@ -37,27 +39,27 @@ export default {
             required: true
         }
     },
-    watch : {
-        tableData(){
+    watch: {
+        tableData() {
             this.initTable()
         }
     },
     components: {
     },
     methods: {
-        nonColoredPositions(item){
+        nonColoredPositions(item) {
             return !this.league.promotion.includes(item.position) && this.league.relegation.filter(a => this.teams.length - a + 1 == item.position).length == 0
         },
-        coloringPosition(item){
+        coloringPosition(item) {
             let coloring = ''
-            if(this.league.promotion.includes(item.position)){
+            if (this.league.promotion.includes(item.position)) {
                 coloring = 'primary white--text'
             }
-            else{
+            else {
                 Array.from(this.league.relegation, i => {
                     let idx = this.teams.length - i + 1 //calculate last indexes for relegated positions
-                    if(idx == item.position){
-                        coloring = 'red white--text' 
+                    if (idx == item.position) {
+                        coloring = 'red white--text'
                     }
                 })
             }
@@ -75,6 +77,7 @@ export default {
                             logo: i.organization.logo_public_url,
                             position: ++idx,
                             score: [0, 0],
+                            matches: 0,
                             points: 0
                         }
                         this.teams.push(team)
@@ -90,22 +93,27 @@ export default {
                 } else if (t.score[0] < t.score[1]) {
                     winner = t.teams[1].name
                     this.teams.filter(f => f.club_name === winner)[0].points += 3
-                } else if (t.score[0] === t.score[1]) { 
+                } else if (t.score[0] === t.score[1]) {
                     let draw = this.teams.filter(f => f.club_name === t.teams[0].name || f.club_name === t.teams[1].name)
-                    draw[0].points +=1
-                    draw[1].points +=1
+                    draw[0].points += 1
+                    draw[1].points += 1
                 }
-                
+
                 // if (w != null) {
                 //     w.points = 3
                 // }
                 Array.from(t.teams, (s, index) => {
-                    if (index == 0) { 
-                        this.teams.filter(f => f.club_name === s.name)[0].score[0] += t.score[0]
-                        this.teams.filter(f => f.club_name === s.name)[0].score[1] += t.score[1]
-                    }else if(index == 1){
-                        this.teams.filter(f => f.club_name === s.name)[0].score[0] += t.score[1]
-                        this.teams.filter(f => f.club_name === s.name)[0].score[1] += t.score[0]
+                    let team = null
+                    if (index == 0) {
+                        team = this.teams.filter(f => f.club_name === s.name)[0]
+                        team.score[0] += t.score[0]
+                        team.score[1] += t.score[1]
+                        team.matches += 1
+                    } else if (index == 1) {
+                        team = this.teams.filter(f => f.club_name === s.name)[0]
+                        team.score[0] += t.score[1]
+                        team.score[1] += t.score[0]
+                        team.matches += 1
                     }
 
                 })
@@ -139,8 +147,9 @@ export default {
         favTeams: ['FK Vechec', 'MFK Vranov nad Topľou'],
         headers: [
             { text: '#', value: 'position' },
-            { text: '', value: 'logo', sortable: false },
+            // { text: '', value: 'logo', sortable: false },
             { text: 'Klub', value: 'club_name' },
+            { text: 'Z', value: 'matches' },
             { text: 'Skóre', value: 'score' },
             { text: 'B', value: 'points' }
         ],
